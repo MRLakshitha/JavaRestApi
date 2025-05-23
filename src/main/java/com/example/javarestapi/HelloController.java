ba0a9b134697a5c9c6e28a0e01ab6a30ce77bc6c
MRLakshitha <lakshithamukkati@gmail.com>
2025-05-23 16:47:30 +0530

    Initial commit

---
+++ src/main/java/com/example/javarestapi/HelloController.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.javarestapi;
+
+/**
+ *
+ * @author Admin
+ */
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello from Java API!";
+    }
+}
